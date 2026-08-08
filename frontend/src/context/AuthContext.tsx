587bc6ef466a1b185eb2b47bf2aa3f8d3096b190
yrk9 <s243151@stn.nagaokaut.ac.jp>
2026-08-08 22:41:41 +0900

    feat: AuthContextの実装

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,6 +1,25 @@
-import { useState, useEffect, createContext, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  Children,
+} from "react";
+import { login as apiLogin } from "../api/auth";
 
-export default function AuthContext() {
+type AuthContextType = {
+  token: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [token, setToken] = useState<string | null>(null); //tokenの状態管理
 
   //ページを開いた瞬間にトークンをstateにわたす
@@ -11,5 +30,26 @@ export default function AuthContext() {
     }
   }, []);
 
-  const AuthContext = createContext();
+  async function login(email: string, password: string) {
+    const data = await apiLogin(email, password);
+
+    localStorage.setItem("token", data.token);
+    setToken(data.token);
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    setToken(null);
+  }
+
+  return (
+    //children: タグの中に書いたのが渡される
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
 }
